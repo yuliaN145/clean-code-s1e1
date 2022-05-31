d9c9018887cdfcd1276afd1a16cd25473db8dc3c
Yulia <Yulia159V@gmail.com>
2022-05-31 09:31:43 +0300

    refactor: change class according to rule 2.1 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ var createNewTaskElement=function(taskString){
     //Each elements, needs appending
     checkBox.type="checkbox";
     editInput.type="text";
-    editInput.className="task";
+    editInput.className="main-content__input";
 
     editButton.innerText="Edit"; //innerText encodes special characters, HTML does not.
     editButton.className="edit";
